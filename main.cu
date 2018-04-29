3d94f5915ffbb1097938dd9ab907c0aea409f432
prime <prime@kmc.gr.jp>
2018-04-30 05:00:56 +0900

    Iterative solve

--- main.cu
+++ main.cu
@@ -6,6 +6,23 @@
 #include "to_board.hpp"
 #include "expand.cuh"
 
+Table init_table() {
+  Table table;
+  constexpr size_t table_size = 50000001;
+  cudaMallocManaged((void**)&table.entries, sizeof(Entry) * table_size);
+  cudaMallocManaged((void**)&table.mutex, sizeof(int) * table_size);
+  cudaMallocManaged((void**)&table.update_count, sizeof(ull));
+  cudaMallocManaged((void**)&table.hit_count, sizeof(ull));
+  cudaMallocManaged((void**)&table.lookup_count, sizeof(ull));
+  table.size = table_size;
+  *table.update_count = 0;
+  *table.hit_count = 0;
+  *table.lookup_count = 0;
+  memset(table.entries, 0, sizeof(Entry) * table_size);
+  memset(table.mutex, 0, sizeof(int) * table_size);
+  return table;
+}
+
 int main(int argc, char **argv) {
   if (argc < 2) {
     std::cerr << "Usage: " << argv[0] << " YBWC_DEPTH" << std::endl;
@@ -15,9 +32,26 @@ int main(int argc, char **argv) {
   ull white = UINT64_C(0x0000001008000000);
   Table2 table;
   Evaluator evaluator("subboard.txt", "value6x6/value");
-  std::vector<AlphaBetaProblem> tasks;
-  float INF = std::numeric_limits<float>::infinity();
-  expand_ybwc(black, white, -INF, INF, table, evaluator, max_depth, tasks);
-  std::cout << tasks.size() << std::endl;
+  Table table_cache = init_table();
+  while (true) {
+    std::vector<AlphaBetaProblem> tasks;
+    constexpr float INF = std::numeric_limits<float>::infinity();
+    float score = expand_ybwc(black, white, -INF, INF, table, evaluator, max_depth, tasks);
+    std::cout << tasks.size() << std::endl;
+    if (tasks.empty()) {
+      std::cout << "Score: " << score << std::endl;
+      break;
+    }
+    BatchedTask batched_task;
+    init_batch(batched_task, tasks.size(), 32 - max_depth, table_cache);
+    memcpy(batched_task.abp, tasks.data(), sizeof(AlphaBetaProblem) * tasks.size());
+    launch_batch(batched_task);
+    while (true) {
+      if (is_ready_batch(batched_task)) break;
+    }
+    for (std::size_t i = 0; i < tasks.size(); ++i) {
+      table[std::make_pair(tasks[i].player, tasks[i].opponent)] = batched_task.result[i];
+    }
+  }
   return 0;
 }
