d9687584538c3a2ede6fdd7adc259246324ebfd1
prime <prime@kmc.gr.jp>
2018-05-01 18:05:07 +0900

    Implement solve_parallel

--- main.cu
+++ main.cu
@@ -1,9 +1,12 @@
 #include "solver.cuh"
+#include <fstream>
 #include <iostream>
 #include <limits>
+#include <random>
 #include <vector>
 #include <boost/timer/timer.hpp>
 #include "to_board.hpp"
+#include "board.cuh"
 #include "expand.cuh"
 
 Table init_table() {
@@ -31,11 +34,122 @@ void destroy_table(Table &table) {
   cudaFree(table.lookup_count);
 }
 
+void solve_parallel(const std::string &in, const std::string &out) {
+  std::ifstream ifs(in);
+  std::size_t n, depth;
+  ifs >> n >> depth;
+  std::vector<std::string> prob_str(n);
+  Table table = init_table();
+  BatchedTask bt;
+  init_batch(bt, n, depth, table);
+  for (std::size_t i = 0; i < n; ++i) {
+    ifs >> prob_str[i];
+    ull player, opponent;
+    std::tie(player, opponent) = toBoard(prob_str[i].c_str());
+    bt.abp[i] = AlphaBetaProblem(player, opponent, -64, 64);
+  }
+  launch_batch(bt);
+  while (true) {
+    if (is_ready_batch(bt)) break;
+  }
+  std::ofstream ofs(out);
+  ofs << n << std::endl;
+  for (std::size_t i = 0; i < n; ++i) {
+    ofs << prob_str[i] << ' ' << bt.result[i] << std::endl;
+  }
+  destroy_batch(bt);
+}
+
+std::string to_base81(ull player, ull opponent) {
+  std::string res;
+  for (int i = 0; i < 16; ++i) {
+    ull p = (player >> (i * 4)) & 0x0F;
+    ull o = (opponent >> (i * 4)) & 0x0F;
+    int coeff[4] = {1, 3, 9, 32};
+    char c = 33;
+    for (int j = 0; j < 4; ++j) {
+      bool isp = (p >> j) & 1;
+      bool iso = (o >> j) & 1;
+      if (isp) {
+        c += coeff[j];
+      } else if (iso) {
+        c += coeff[j] * 2;
+      }
+    }
+    res += c;
+  }
+  return res;
+}
+
+std::pair<ull, bool> select(ull player, ull opponent, std::mt19937 &mt) {
+  ull mobility_bits = mobility(player, opponent);
+  bool pass = false;
+  if (mobility_bits == 0) {
+    pass = true;
+    mobility_bits = mobility(opponent, player);
+  }
+  const int count = __builtin_popcountll(mobility_bits);
+  if (count == 0) return std::make_pair(0ull, true);
+  std::uniform_int_distribution<int> dis(0, count-1);
+  const int index = dis(mt);
+  for (int i = 0; mobility_bits; mobility_bits &= mobility_bits - 1, ++i) {
+    if (i == index) {
+      return std::make_pair(mobility_bits & -mobility_bits, pass);
+    }
+  }
+  return std::make_pair(0ull, true);
+}
+
+bool make_prob_impl(std::size_t depth, std::mt19937 &mt) {
+  ull player = UINT64_C(0x0000000810000000);
+  ull opponent = UINT64_C(0x0000001008000000);
+  for (std::size_t i = 0; i < depth; ++i) {
+    ull pos_bit;
+    bool pass;
+    std::tie(pos_bit, pass) = select(player, opponent, mt);
+    if (pos_bit == 0) return false;
+    int pos = __builtin_popcountll(pos_bit - 1);
+    if (!pass) {
+      ull flip_bits = flip(player, opponent, pos);
+      ull next_player = opponent ^ flip_bits;
+      ull next_opponent = (player ^ flip_bits) | pos_bit;
+      player = next_player;
+      opponent = next_opponent;
+    } else {
+      ull flip_bits = flip(opponent, player, pos);
+      ull next_player = player ^ flip_bits;
+      ull next_opponent = (opponent ^ flip_bits) | pos_bit;
+      player = next_player;
+      opponent = next_opponent;
+    }
+  }
+  std::cout << to_base81(player, opponent) << std::endl;
+  return true;
+}
+
+void make_prob(std::size_t depth, std::size_t count) {
+  std::random_device rd;
+  std::mt19937 mt(rd());
+  for (std::size_t i = 0; i < count; ) {
+    if (make_prob_impl(depth, mt)) ++i;
+  }
+}
+
 int main(int argc, char **argv) {
   if (argc < 2) {
     std::cerr << "Usage: " << argv[0] << " YBWC_DEPTH" << std::endl;
   }
-  size_t max_depth = std::strtoul(argv[1], nullptr, 10);
+  char *ptr = nullptr;
+  size_t max_depth = std::strtoul(argv[1], &ptr, 10);
+  if (ptr == argv[1]) {
+    solve_parallel(argv[1], argv[2]);
+    return 0;
+  }
+  if (argc >= 3) {
+    std::size_t count = std::strtoul(argv[2], nullptr, 10);
+    make_prob(max_depth, count);
+    return 0;
+  }
   ull black = UINT64_C(0x0000000810000000);
   ull white = UINT64_C(0x0000001008000000);
   Table2 table;
