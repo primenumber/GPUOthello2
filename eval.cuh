5e15d18ccb9b9cf476cd6a12ba97ce8f9b0751dd
prime <prime@kmc.gr.jp>
2018-05-06 02:11:15 +0900

    Fix parameter name

--- eval.cuh
+++ eval.cuh
@@ -6,9 +6,9 @@ class Evaluator {
  public:
   __host__ Evaluator() {};
   __host__ Evaluator(const std::string &features_file_name, const std::string &values_file_name);
-  __host__ __device__ float eval(const ull op, const ull me);
+  __host__ __device__ float eval(const ull me, const ull op);
  private:
-  __host__ __device__ int get_index(const ull op, const ull me, const ull feature);
+  __host__ __device__ int get_index(const ull me, const ull op, const ull feature);
   size_t features_count;
   float **values;
   float offset;
