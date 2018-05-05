43e10932007bb7eeea2bfdc61ef433f0f0175340
prime <prime@kmc.gr.jp>
2018-05-05 21:15:39 +0900

    Fix value table file name

--- main.cu
+++ main.cu
@@ -153,7 +153,7 @@ int main(int argc, char **argv) {
   ull black = UINT64_C(0x0000000810000000);
   ull white = UINT64_C(0x0000001008000000);
   Table2 table;
-  Evaluator evaluator("subboard6x6.txt", "value6x6/value");
+  Evaluator evaluator("subboard6x6.txt", "value6x6/value16_b");
   Table table_cache = init_table();
   while (true) {
     std::vector<AlphaBetaProblem> tasks;
